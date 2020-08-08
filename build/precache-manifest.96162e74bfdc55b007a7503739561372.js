8dad3d424774c98b54e3f7f6e0994f786a9f2783
Dylan Sawyer <dylan.sawyer@luckie.com>
2020-08-07 20:05:39 -0400

    adding build folder'

---
+++ build/precache-manifest.96162e74bfdc55b007a7503739561372.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a22d1c15aa1b365ea12f3ba077cccba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "433ef5755de10d651c91",
+    "url": "/static/css/main.c2d10f59.chunk.css"
+  },
+  {
+    "revision": "5edfa753c6a3aecfb234",
+    "url": "/static/js/2.81f1d061.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.81f1d061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433ef5755de10d651c91",
+    "url": "/static/js/main.f4ccaa14.chunk.js"
+  },
+  {
+    "revision": "22c3aed477cdc0ed73a7",
+    "url": "/static/js/runtime-main.d6828ab7.js"
+  },
+  {
+    "revision": "6f185a89b29d27df436ab1699c58d83c",
+    "url": "/static/media/CS1.6f185a89.png"
+  },
+  {
+    "revision": "5d5a061b84d52b81a0ce4bf5a66935c2",
+    "url": "/static/media/FT.5d5a061b.png"
+  },
+  {
+    "revision": "7426f2f5688b1b6a9710751a90aa58e0",
+    "url": "/static/media/PG1.7426f2f5.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "68cc776bdd925c39995527bb2fc5765c",
+    "url": "/static/media/Shrikhand-Regular.68cc776b.ttf"
+  },
+  {
+    "revision": "d30ef536a512cb507af5b3fa5c7e7067",
+    "url": "/static/media/WP1.d30ef536.png"
+  },
+  {
+    "revision": "0d54aa7b297a81cec03408bc12c4c9bd",
+    "url": "/static/media/me.0d54aa7b.jpg"
+  }
+]);
