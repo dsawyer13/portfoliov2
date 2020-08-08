1410979bbdb7333e2ce0fb304a6a90268003786b
Dylan Sawyer <dylan.sawyer@luckie.com>
2020-08-07 20:10:17 -0400

    Updates

---
+++ precache-manifest.2a5b0cbea2cd336fb09229d03e3aac33.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291b41ba04faa3b4fdaffd2a2f6de67f",
+    "url": "/portfoliov2/index.html"
+  },
+  {
+    "revision": "92dd197ab11e3c52b8f1",
+    "url": "/portfoliov2/static/css/main.443db7e0.chunk.css"
+  },
+  {
+    "revision": "5edfa753c6a3aecfb234",
+    "url": "/portfoliov2/static/js/2.81f1d061.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfoliov2/static/js/2.81f1d061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dd197ab11e3c52b8f1",
+    "url": "/portfoliov2/static/js/main.b24a2a11.chunk.js"
+  },
+  {
+    "revision": "62c1058e454e7a8e7afd",
+    "url": "/portfoliov2/static/js/runtime-main.f427dd8a.js"
+  },
+  {
+    "revision": "6f185a89b29d27df436ab1699c58d83c",
+    "url": "/portfoliov2/static/media/CS1.6f185a89.png"
+  },
+  {
+    "revision": "5d5a061b84d52b81a0ce4bf5a66935c2",
+    "url": "/portfoliov2/static/media/FT.5d5a061b.png"
+  },
+  {
+    "revision": "7426f2f5688b1b6a9710751a90aa58e0",
+    "url": "/portfoliov2/static/media/PG1.7426f2f5.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/portfoliov2/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/portfoliov2/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/portfoliov2/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "68cc776bdd925c39995527bb2fc5765c",
+    "url": "/portfoliov2/static/media/Shrikhand-Regular.68cc776b.ttf"
+  },
+  {
+    "revision": "d30ef536a512cb507af5b3fa5c7e7067",
+    "url": "/portfoliov2/static/media/WP1.d30ef536.png"
+  },
+  {
+    "revision": "0d54aa7b297a81cec03408bc12c4c9bd",
+    "url": "/portfoliov2/static/media/me.0d54aa7b.jpg"
+  }
+]);
